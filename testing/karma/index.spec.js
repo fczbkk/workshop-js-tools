89334bc90fcbee3b0ebc930c2a02c59f68b5be4d
fczbkk <riki@fczbkk.com>
2019-02-03 07:33:42 +0100

    add async test example

--- testing/karma/index.spec.js
+++ testing/karma/index.spec.js
@@ -1,9 +1,16 @@
-import {add} from './calculator'
+import {add, doComplicatedMath} from './calculator'
 
 describe('Calculator', function () {
 
   it('should add two numbers', function () {
     assert.equal(add(1, 2), 3);
+  });
+
+  it('should do some complicated math', function (done) {
+    doComplicatedMath(1, function (result) {
+      assert.equal(result, 1);
+      done();
+    });
   });
 
 });
