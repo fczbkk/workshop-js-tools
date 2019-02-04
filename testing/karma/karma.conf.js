17a9438ec57de583363a7cb09778ae173aec8635
fczbkk <riki@fczbkk.com>
2019-02-04 07:49:04 +0100

    fix: cleanup Karma example

--- testing/karma/karma.conf.js
+++ testing/karma/karma.conf.js
@@ -24,11 +24,11 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    autoWatch: false,
     browsers: [
       'PhantomJS',
       'ChromeHeadless'
     ],
-    singleRun: false
+    singleRun: true
   })
 }
