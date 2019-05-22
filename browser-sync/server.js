35ec94dd7aff865b03718a8176373fab65328d04
Riki Fridrich <riki@fczbkk.com>
2019-05-22 07:09:56 +0200

    fix path to source in browser-sync example

--- browser-sync/server.js
+++ browser-sync/server.js
@@ -5,5 +5,6 @@ const port = 3000
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'index.html')))
 app.use('/src', express.static('src'))
+app.use('/build', express.static('build'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
