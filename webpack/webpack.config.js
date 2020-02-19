04e88a8bfd651c24316de87c2a6576d03719853e
Riki Fridrich <riki@fczbkk.com>
2020-02-19 07:30:09 +0100

    add environment check to webpack config

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const autoprefixer = require('autoprefixer')
 
 module.exports = {
-  mode: 'development',
+  mode: (process.env.NODE_ENV === 'production') ? 'production' : 'development',
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     path: path.resolve(__dirname, 'build'),
